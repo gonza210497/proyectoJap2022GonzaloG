944e33bdb2ddb274762893d1e1e1e5e772042fe5
gonza210497 <110430522+gonza210497@users.noreply.github.com>
2022-11-07 22:04:26 -0300

    agrega funcionalidad a validaciones

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,27 +8,48 @@ document.addEventListener("DOMContentLoaded", function (e) {
         e.stopPropagation();
 
         let primerNombre = document.getElementById("primerNombre")
+        let segundoNombre = document.getElementById("segundoNombre")
         let primerApellido = document.getElementById("primerApellido")
+        let segundoApellido = document.getElementById("segundoApellido")
+        let contacto = document.getElementById("contacto")
         let email = document.getElementById("email")
         let paso = true;
 
         primerNombre.classList.remove('is-invalid');
         primerApellido.classList.remove('is-invalid');
-        
+        email.classList.remove('is-invalid');
 
         if (primerNombre.value === "") {
             primerNombre.classList.add('is-invalid');
             paso = false;
+        }else {
+
+            primerNombre.classList.add('is-valid')
         }
+        if (segundoNombre.value === ""){
+
+        }else{}
 
         if (primerApellido.value === "") {
             primerApellido.classList.add('is-invalid');
             paso = false;
+        }else {
+            primerApellido.classList.add('is-valid');
         }
+        if (segundoApellido.value === ""){
+
+        }else {}
 
         if (email.value === "") {
             email.classList.add('is-invalid');
             paso = false;
+        }else {
+            email.classList.add('is-valid');
         }
+        if (contacto.value === ""){
+
+        }else{}
+
+        
     })
 })
